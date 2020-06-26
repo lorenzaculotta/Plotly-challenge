84a43da8c0abcd0c29d9bfa5db135ca5ffc34f82
LorenzaC <lorenza.culotta@northwestern.edu>
2020-06-25 23:21:49 -0500

    updated files

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,94 +1,166 @@
-// // Unpack function
-// function unpack(rows, index) {
-//     return rows.map(function(row) {
-//       return row[index];
-//     });
-//   }
-
-// Load samples file
-d3.json("samples.json").then((data) => {
-    // print entire dataset    WORKING
-    var dataset= data.samples
-    // console.log(dataset)
-
-    // print ids  WORKING
-    var ids= dataset.map(d => d.id)
-    // console.log(ids)
-
-    // get 940 ID 
-    var filteredDataset= dataset.filter(d => d.id === "940")
-    console.log(filteredDataset)
-
-    // get otu_ids  WORKING
-    var otuIds= filteredDataset.map(d => d.otu_ids.slice(0,10))[0]
-    console.log(otuIds)
-
-    // add "OTU" to each otu_id  WORKING
-    var otuIdLong= otuIds.map(d => `OTU ${d}`)
-    console.log(otuIdLong)
-
-    // get sample_values WORKING
-    var sampleValues=filteredDataset.map(d => d.sample_values.slice(0,10))[0]
-    console.log(sampleValues[0])
-
-    // get otu_labels WORKING
-    var otuLabels= filteredDataset.map(d => d.otu_labels.slice(0,10))[0]
-    console.log(otuLabels)
-
-
-    // Trace1 
-    var trace1 = {
-        x: sampleValues.reverse(),
-        y: otuIdLong.reverse(),
-        text: otuLabels,
-        type: "bar",
-        orientation: "h"
-        };
+//READ SAMPLE.JSON FILE
+d3.json("samples.json").then((bellyButtonData) => {
     
-    // data
-    var data = [trace1];
+    // retrieve data on samples
+    var dataset= bellyButtonData.samples
 
-    // Apply the group bar mode to the layout
-    var layout = {
-        xaxis: {title: "Sample values"},
-        // margin: {
-        //     l: 100,
-        //     r: 100,
-        //     t: 100,
-        //     b: 100
-        // }
+    // POPULATE DROPDOWN MENU
+    // retrieve ids  
+    var ids= dataset.map(d => d.id)
+    // create an option for each id
+    ids.forEach(function(d) {
+        d3.selectAll("#selDataset")
+        .append("option")
+        .text(d);
+    });
+
+    // DROPDOWN EVENTS
+    // On change to the DOM, call updateId()
+    d3.selectAll("#selDataset").on("change", updateId);
+
+    // Create a function to handle the event
+    function updateId () {
+        var dropdownMenu= d3.select("#selDataset");
+        // assign value of dropdown menu to a variable
+        var inputId= dropdownMenu.property("value"); 
+        // plot bar and bubble chart and update metadata
+        plotBar(inputId);
+        plotBubble(inputId);
+        displayMetadata(parseInt(inputId));
+    }
+    
+    // -------------------------------------------
+    // FUNCTION TO FILTER DATA AND RETRIEVE INFO
+    function filterData(chosenId) {
+
+        // filter dataset with id selected in the dropdown menu
+        var filteredDataset= dataset.filter(d => d.id === chosenId)
+
+        // get otu_ids and slice top 10 
+        var otuIds= filteredDataset.map(d => d.otu_ids)
+        var slicedOtuIds= otuIds[0].slice(0,10)
+
+        // add "OTU" to each otu_id  (will need this for bar chart)
+        var otuIdLong= slicedOtuIds.map(d => `OTU ${d}`)
+
+        // get sample_values and slice top 10 
+        var sampleValues=filteredDataset.map(d => d.sample_values)                                     
+        var slicedSampleValues= sampleValues[0].slice(0,10)
+
+        // get otu_labels and slice top 10 
+        var otuLabels= filteredDataset.map(d => d.otu_labels)
+        var slicedOtuLabels= otuLabels[0].slice(0,10)
+
+        return ([slicedOtuIds, otuIdLong, slicedSampleValues, slicedOtuLabels]);
+    };
+    
+    // -------------------------------------------
+    // FUNCTION TO CREATE BAR CHART
+    function plotBar(chosenId) {
+        // run filter data function and return: [0]slicedOtuIds, [1]otuIdLong, [2]slicedSampleValues, [3]slicedOtuLabels
+        
+        // plot
+        var trace1 = {
+            x: filterData(chosenId)[2].reverse(),
+            y: filterData(chosenId)[1].reverse(),
+            text: filterData(chosenId)[3],
+            type: "bar",
+            orientation: "h"    
+        };
+        
+        // data
+        var dataBar = [trace1];
+
+        // Apply the group bar mode to the layout
+        var layoutBar = {
+            title: "Bar Chart of TOP10 OTU for selected Subject ID",
+            xaxis: {title: "Sample values"},
+            yaxis: {title: "Operational Taxonomic Units (OTU)"},
+            height: 600,
+            width: 1000,
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100
+            }
+        };
+
+        // Render the plot to the div tag with id "plot"
+        Plotly.newPlot("bar", dataBar, layoutBar);
     };
 
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("bar", data, layout);
+    // -------------------------------------------
+    // FUNCTION TO PLOT BUBBLE CHART
+    function plotBubble(chosenId) {
+        // run filter data function and return: [0]slicedOtuIds, [1]otuIdLong, [2]slicedSampleValues, [3]slicedOtuLabels
+        
+        // plot
+        var trace2 = {
+            x: filterData(chosenId)[0], 
+            y: filterData(chosenId)[2],
+            text: filterData(chosenId)[3],
+            mode: "markers",
+            marker: {
+                size: filterData(chosenId)[2],
+                color: filterData(chosenId)[0],
+                opacity: 0.8
+            }
+        };
+        
+        // data
+        var dataBubble = [trace2];
 
-    // Call updatePlotly() when a change takes place to the DOM
-    // d3.selectAll("#selDataset").on("change", updatePlotly);
+        // Apply the group bar mode to the layout
+        var layoutBubble = {
+            title: "Bubble Chart of TOP10 OTU for selected Subject ID",
+            xaxis: {title: "OTU ID"},
+            yaxis: {title: "Sample values"},
+            height: 800,
+            width: 1150,
+            margin: {
+                l: 50,
+                r: 50,
+                t: 100,
+                b: 100
+            }
+        };
+
+        // Render the plot to the div tag with id "plot"
+        Plotly.newPlot("bubble", dataBubble, layoutBubble);
+    }
+
+
+    // -------------------------------------------
+    // FUNCTION TO DISPLAY METADATA 
+    function displayMetadata(chosenId) {
+
+        // retrieve metadata
+        var metadata= bellyButtonData.metadata
+
+        // remove all info in demographic panel if exists
+        d3.selectAll(".panel-body > h5").remove()
+
+        // filter metadata according to chosen id
+        var filteredMetadata= metadata.filter(d => d.id === chosenId)[0]
+        
+        // get key-value pairs and add them to the demographic info panel
+        Object.entries(filteredMetadata).forEach(function([key, value]) {
+            d3.selectAll(".panel-body").append("h5").html("<strong>" + key + ": " + value + "<strong>");
+        });
+    };          
+    
+
+    // -------------------------------------------
+    // FUNCTION TO INITIALIZE
+    function init() {
+        plotBar("940");
+        plotBubble("940");
+        displayMetadata(940);
+    }
+    
+    // call function to initialize
+    init();
 
 });
 
-// // This function is called when a dropdown menu item is selected
-// function updatePlotly() {
-//     // Use D3 to select the dropdown menu
-//     var dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     var dataset = dropdownMenu.property("value");
-  
-//     // Initialize x and y arrays
-//     var x = [];
-//     var y = [];
-  
-//     if (dataset === 'dataset1') {
-//       x = [1, 2, 3, 4, 5];
-//       y = [1, 2, 4, 8, 16];
-//     }
-  
-//     if (dataset === 'dataset2') {
-//       x = [10, 20, 30, 40, 50];
-//       y = [1, 10, 100, 1000, 10000];
-//     }
-  
-//     // Note the extra brackets around 'x' and 'y'
-//     Plotly.restyle("plot", "x", [x]);
-//     Plotly.restyle("plot", "y", [y]);
-//   }
